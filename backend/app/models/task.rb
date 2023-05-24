3013a88ecadf8432e364ad927c4dab2d8a949203
root <root@maymine>
2023-05-24 08:35:08 -0500

    created table user and relation among user and task

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ApplicationRecord
     validates :description, presence: true
     validates :start_date, presence: true
     validates :end_date,  presence: true
+
+    belongs_to :user, default:  -> { Current.user }
 end
